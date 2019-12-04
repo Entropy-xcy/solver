8109517ab1b081fac17ccc84c3b0d588dab6cfcf
Entropy Xu <entropy@Entropy.local>
2019-12-03 17:20:55 -0800

    Add solver

--- solver.py
+++ solver.py
@@ -53,7 +53,9 @@ solution['sum'] = []
 solution['similar'] = []
 
 # Add Truth to the geometry predicates
-geometric_predicates['parallel'] = []
+geometric_predicates['parallel'] = [['e1b', 'e2b', 'e6b'], ['e5a', 'e4b', 'e3a', 'e7a', 'e8a'],
+                                    ['e5c', 'e1a', 'e7c', 'e8d'], ['e3b', 'e8c', 'e7b'],
+                                    ['e1c', 'e8e', 'e4c', 'e6c'], ['e4a', 'e3c', 'e2a', 'e8b', 'e6a']]
 geometric_predicates['perpendicular'] = []
 geometric_predicates['equal'] = [['a5a', 'a7c'], ['a6c', 'a1c'],
                                  ['a2b', 'a6b'], ['a3b', 'a7a'],
@@ -69,12 +71,48 @@ geometric_predicates['sum'] = [['a1a', 'a8e', 180], ['a8e', 'a5c', 180],
 geometric_predicates['similar'] = []
 
 
-
 for i in range(1, 8):
     triangles.append(triangle(str(i)))
 for t in triangles:
     all_elements += t.flatten()
 print(all_elements)
+
+
+def delete_repetition(sett):
+    return list(set(sett))
+
+
+def is_subset_of(sub, big_set):
+    flag = False
+    if all(x in big_set for x in sub):
+        flag = True
+    return flag
+
+
+def is_subsubset_of(sub, big_set):
+    for ele in big_set:
+        if is_subset_of(sub, ele):
+            return True
+    return False
+
+
+def has_subset_of(big_subsetset, set):
+    for sub in big_subsetset:
+        if is_subset_of(sub, set):
+            return True
+    return False
+
+
+def has_element_of(big_subset, ele):
+    flattened = []
+    for i in big_subset:
+        for j in i:
+            flattened.append(j)
+    for i in ele:
+        if i in flattened:
+            return True
+    return False
+
 
 def set_parallel(name1, name2):
     geometric_predicates["parallel"].append([name1, name2])
@@ -134,6 +172,55 @@ def test_sum():
     pass
 
 
+def infer_parallel(sensitive_elements):
+    sols = []
+    parallels = geometric_predicates['parallel']
+    sensitive_pairs = []
+    for p in parallels:
+        for s in sensitive_elements:
+            if s in p:
+                sensitive_pairs.append(p)
+    for p in sensitive_pairs:
+        if is_subsubset_of(p, sols):
+            pass
+        elif has_element_of(sols, p):
+            for s in sols:
+                for ele in s:
+                    if ele in p:
+                        s += p
+                        delete_repetition(s)
+        else:
+            sols.append(p)
+    return sols
+
+
+def test_parallel():
+    # Test Parallel First
+    parallels = geometric_predicates['parallel']
+    flatten = []
+    for i in parallels:
+        for j in i:
+            flatten.append(j)
+
+    for elements in flatten:
+        count = 0
+        sols = []
+        for i in range(len(parallels)):
+            if elements in parallels[i]:
+                count += 1
+        if count > 1:
+            set_elements = []
+            for pairs in parallels:
+                if elements in pairs:
+                    set_elements += pairs
+                    set_elements = delete_repetition(set_elements)
+            sols += set_elements
+        print(sols)
+        if sols != []:
+            solution['parallel'].append(sols)
+            geometric_predicates['parallel'].append(sols)
+
+
 def test_angle():
     perpendiculars = geometric_predicates['perpendicular']
     for per_instance in perpendiculars:
@@ -142,6 +229,7 @@ def test_angle():
 
 def solve():
     test_equal()
+    test_parallel()
     test_angle()
 
 
@@ -152,6 +240,7 @@ def get_all():
 
 def main():
     set_equal('a5b', 'a5c')
+    set_parallel('e7a', 'e2b')
     print(get_all())
 
 
